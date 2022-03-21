987d3c8ca45dff6ec4f557e0356354fd1613a393
Abarna Pandian <abarnapandian2000@gmail.com>
2022-03-21 20:46:33 +0530

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,15 @@
+public class variables
+{
+	public static void main(String args[])
+	{
+		int num = 5;
+		float percent = 6.89f;
+		double percent1 = 78.90;
+		System.out.println(num);
+		System.out.println(percent);
+		System.out.println(percent1);
+	}
+	
+	
+
+}
