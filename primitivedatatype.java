5389558510c79ce658322f6eb194bc91a43358a4
Abarna Pandian <abarnapandian2000@gmail.com>
2022-03-21 21:19:25 +0530

    Create primitivedatatype.java

---
+++ primitivedatatype.java
@@ -0,0 +1,32 @@
+
+public class primitivedatatype
+{
+	public static void main(String args[])
+	{
+		byte a = 109; //1 byte -> 8 bits -> -128 to 127
+		short mynum = 5000; //2 bytes -> 16bits -> -32,768 to 32,767
+		int mynum1 = 100000; //4 bytes -> 32 bits ->  -2,147,483,648 to 2,147,483,647
+		long mynum2 = 15000000000L; //8 bytes -> 64bits ->-9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+		float mynum3 = 5.75f; //4 bytes -> 32 bits ->Sufficient for storing 6 to 7 decimal digits
+		double mynum4 = 19.99d; //8 bytes -> 64bits ->Sufficient for storing 15 decimal digits
+		boolean isjavafun = true; //1 byte -> Stores true or false values
+		char mygrade = 'B'; //2 bytes -> 16 bits ->Stores a single character/letter or ASCII values
+		mygrade = 68;
+		System.out.println(a);
+		System.out.println(mynum);
+		System.out.println(mynum1);
+		System.out.println(mynum2);
+		System.out.println(mynum3);
+		System.out.println(mynum4);
+		System.out.println(isjavafun);
+		System.out.println(mygrade);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
