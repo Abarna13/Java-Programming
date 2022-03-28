d44ccae9daa486236045c0e4898a71eeaca17337
Abarna Pandian <abarnapandian2000@gmail.com>
2022-03-28 23:55:58 +0530

    Create PrintfMethod.java

---
+++ PrintfMethod.java
@@ -0,0 +1,11 @@
+public class PrintfMethod
+{
+  public static void main(String args[])
+  {
+    int i,j,k;
+    i = 7;
+    j = 5;
+    k = i + j;
+    System.out.printf("The Addition of %d and %d is %d",i,j,k);
+  }
+}
