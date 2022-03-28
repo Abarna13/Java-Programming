3d3dcc51d8b0f700485302758f2d51d7d64d4f3c
Abarna Pandian <abarnapandian2000@gmail.com>
2022-03-29 00:30:51 +0530

    Create ShortHandOperators.java

---
+++ ShortHandOperators.java
@@ -0,0 +1,36 @@
+
+public class ShortHandOperaors {
+
+	public static void main(String[] args) {
+		
+		//Short hand operators  
+	    int i=78;
+	    i += 3; //i = i+3
+	    System.out.println(i);
+	    
+	    i = 65;
+	    i -= 4;//i=i-4
+	    System.out.println(i);
+	    
+	    i = 4;
+	    i *= 9;//i=i*9
+	    System.out.println(i);
+	    
+	    
+	    i = 36;
+	    i /= 6; //i = i/6
+	    System.out.println(i);
+	    
+	    i = 38;
+	    i %= 6; //i = i%6
+	    System.out.println(i);
+	    
+	    
+	    
+	    
+	    
+	    
+
+	}
+
+}
