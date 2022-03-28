c334e9f71355bcde4fde74ceae8e37618e1f3cd9
Abarna Pandian <abarnapandian2000@gmail.com>
2022-03-28 23:53:58 +0530

    Create PrintlnMethod.java

---
+++ PrintlnMethod.java
@@ -0,0 +1,11 @@
+public class PrintlnMethod
+{
+  public static void main(String args[])
+  {
+    int i,j,k;
+    i = 7;
+    j = 5;
+    k = i + j;
+    System.out.println("The Addition of "+i + " and "+j + " is "+k);
+  }
+}
