112bbb59ac035f2d9c18ee7414c63d4258a42328
Abarna Pandian <abarnapandian2000@gmail.com>
2022-04-04 22:12:44 +0530

    Create PostIncrement.java

---
+++ PostIncrement.java
@@ -0,0 +1,14 @@
+public class PostIncrement {
+
+	public static void main(String[] args) {
+		
+		int i = 8;
+		
+		int j = i++;
+		
+		System.out.println(j);
+		System.out.println(j + " : " + i);
+		
+	}
+
+}
