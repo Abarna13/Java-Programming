c276ece37b246fe07cc15832b32d1ff765a97061
Abarna Pandian <abarnapandian2000@gmail.com>
2022-04-04 22:14:15 +0530

    Create PreIncrement.java

---
+++ PreIncrement.java
@@ -0,0 +1,15 @@
+public class PreIncrement {
+
+	public static void main(String[] args) {
+		int i = 8;
+		
+		int j = ++i;
+		
+		System.out.println(j);
+		System.out.println(j + " : " + i);
+		
+		
+
+	}
+
+}
