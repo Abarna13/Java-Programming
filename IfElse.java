b61df77b97dc544bbed94b07767f1cbd81809598
Abarna Pandian <abarnapandian2000@gmail.com>
2022-04-07 00:08:27 +0530

    Create IfElse.java

---
+++ IfElse.java
@@ -0,0 +1,17 @@
+public class IfElse
+{
+	public static void main(String[] args)
+	{
+		int num = 9;
+		if(num%2==0)
+		{
+			System.out.println("Even Number");
+		}
+		else
+		{
+			System.out.println("Odd Number");
+		}
+
+	}
+
+}
