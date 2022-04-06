869052ac53a43ad1089059e69f7c182c299630be
Abarna Pandian <abarnapandian2000@gmail.com>
2022-04-06 23:39:18 +0530

    Create Boolean.java

---
+++ Boolean.java
@@ -0,0 +1,11 @@
+public class Boolean
+{
+
+	public static void main(String[] args)
+	{
+		boolean a = true;
+		a = !a; //! symbol is used for inverting the variable a
+		System.out.println(a);		
+	}
+
+}
