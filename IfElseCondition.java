efb4c1db722dee841dd9885c183120c647c02d2a
Abarna Pandian <abarnapandian2000@gmail.com>
2022-04-07 00:09:06 +0530

    Create IfElseCondition.java

---
+++ IfElseCondition.java
@@ -0,0 +1,20 @@
+public class IfElseCondition {
+
+	public static void main(String[] args)
+	{
+		int a = 6, b = 4, c = 10;
+		if(a>b && a>c)
+		{
+			System.out.println("A is greater");
+		}
+		else if(b>c)
+		{
+			System.out.println("B is greater");
+		}
+		else
+		{
+			System.out.println("C is greater");
+		}
+		
+
+	}
