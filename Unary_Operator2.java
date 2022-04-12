c6e33911e348859d167ed3e5c9fd30d381f54779
Abarna Pandian <abarnapandian2000@gmail.com>
2022-04-12 22:15:13 +0530

    Create Unary_Operator2.java

---
+++ Unary_Operator2.java
@@ -0,0 +1,12 @@
+public class Unary_Operator2 {
+
+	public static void main(String[] args)
+	{
+		boolean bvalue = false;
+		System.out.println(bvalue);
+		System.out.println(!bvalue); //It's a logical complement operator in unary operator
+		
+
+	}
+
+}
