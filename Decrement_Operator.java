4590dc8cf36c587c6d1fe3a6de54cba197066b41
Abarna Pandian <abarnapandian2000@gmail.com>
2022-04-12 22:26:31 +0530

    Create Decrement_Operator.java

---
+++ Decrement_Operator.java
@@ -0,0 +1,19 @@
+
+public class Decrement_Operator {
+
+	public static void main(String[] args)
+	{
+		int age = 38;
+		age = age--;
+		System.out.println(age);
+		
+		
+		age = --age;
+		System.out.println(age);
+		
+		System.out.println(age-- + --age);
+		
+
+	}
+
+}
