24f913bae179560a47e2971666300fef3fc2b15c
Abarna Pandian <abarnapandian2000@gmail.com>
2022-04-12 22:13:04 +0530

    Create UnaryOperators.java

---
+++ UnaryOperators.java
@@ -0,0 +1,17 @@
+public class UnaryOperators
+{
+
+	public static void main(String[] args)
+	{
+	    int num = 6;
+	    num = +num;
+	    System.out.println(num);
+	    
+	    
+	    num = -num;
+	    System.out.println(num);
+	    
+	    
+	}
+
+}
