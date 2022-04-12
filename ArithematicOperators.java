67b93ae72b0ee7d999fc5612d7a2ac05409d9860
Abarna Pandian <abarnapandian2000@gmail.com>
2022-04-12 12:15:39 +0530

    Create ArithematicOperators.java

---
+++ ArithematicOperators.java
@@ -0,0 +1,27 @@
+
+public class ArithematicOperators {
+
+	public static void main(String[] args)
+	{
+		int num1 = 7, num2 = 9, sum = 0, sub= 0,mul = 0, div = 0, mod = 0;
+		
+		sum = num1 + num2;
+		System.out.println("sum is " + sum);
+		
+		sub = num1 - num2;
+		System.out.println("sub is " + sub);
+		
+		mul = num1 * num2;
+		System.out.println("Multiplication is " + mul);
+		
+		div = num1 / num2;
+		System.out.println("Division is " + div);
+		
+		mod = num1 % num2;
+		System.out.println("Remainder is " + mod);
+		
+		
+
+	}
+
+}
