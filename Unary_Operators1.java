3f731da5b70aa50c1cda6123770a58f29ff94d44
Abarna Pandian <abarnapandian2000@gmail.com>
2022-04-12 22:14:04 +0530

    Create Unary_Operators1.java

---
+++ Unary_Operators1.java
@@ -0,0 +1,23 @@
+public class Unary_Operators1
+{
+
+	public static void main(String[] args)
+	{
+		int num = 6;
+		num++; //post increment
+		System.out.println(num);
+		
+		++num; //pre-increment
+		System.out.println(num);
+		
+		
+		num--; //post decrement
+		System.out.println(num);
+		
+		--num; //pre-decrement
+		System.out.println(num);
+		
+
+	}
+
+}
