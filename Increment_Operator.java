e18e930d8ea23dc456b8efe1a997d0131c4f0f03
Abarna Pandian <abarnapandian2000@gmail.com>
2022-04-12 22:27:18 +0530

    Create Increment_Operator.java

---
+++ Increment_Operator.java
@@ -0,0 +1,19 @@
+
+public class Increment_Operator {
+
+	public static void main(String[] args)
+	{
+		int age = 34;
+		age = age++;
+		System.out.println(age);
+		
+		
+		age = ++age;
+		System.out.println(age);
+		
+		System.out.println(age++ + ++age);
+		
+
+	}
+
+}
