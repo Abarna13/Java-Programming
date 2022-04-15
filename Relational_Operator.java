e286d0154ed2e025bb7c26a46602f7cd76a6d3f6
Abarna Pandian <abarnapandian2000@gmail.com>
2022-04-15 22:52:19 +0530

    Create Relational_Operator.java

---
+++ Relational_Operator.java
@@ -0,0 +1,29 @@
+
+public class Relational_Operator
+{
+
+	public static void main(String[] args)
+	{
+		int a = 6, b=9;
+		System.out.println("a is " + a + " and b is " +b);
+		
+		System.out.println(a == b);
+		
+		System.out.println(a != b);
+		
+		System.out.println(a > b);
+		
+		System.out.println(a < b);
+		
+		System.out.println(a >= b);
+		
+		System.out.println(a <= b);
+		
+		
+
+
+		
+
+	}
+
+}
